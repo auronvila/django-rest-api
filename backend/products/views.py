acde56b87fd4e4533dd9472dc3c69e2d48fee757
auronvila <auronvila1121@gmail.com>
2024-08-04 23:15:27 +0300

    created product detail api view and create using django rest framework

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductCreateApiView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content')
+        if content is None:
+            content = title
+        serializer.save()
+
+
+product_create_detail_view = ProductCreateApiView.as_view()
+
+
+class ProductDetailApiView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+product_detail_view = ProductDetailApiView.as_view()
